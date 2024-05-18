0e8a34d3af4bc245715e54cee333ba3f48da1d61
Jean Soleil <116062964+jansole@users.noreply.github.com>
2024-05-18 15:55:49 +0200

    Update train.py

--- Ferros/train.py
+++ Ferros/train.py
@@ -1,5 +1,5 @@
 class Train:
-    def __init__(self, line, ID, direction, stops, train_type, position, m1, m2, ri, mi):
+    def __init__(self, line, ID, direction, stops, train_type, position, m1, m2, ri = 0, mi):
         self._line = line  # Linia (L7, S2, RL1...)
         self._ID = ID  # ID tren
         self._direction = direction  # D/A
@@ -7,6 +7,11 @@ class Train:
         self._train_type = train_type  # tipus tren (113/114/115 etc)
         self._position = position  # geolocalització
         self._carriers = {'M1': m1, 'M2': m2, 'RI': ri, 'MI': mi}  # Carrier % status
+
+        """Només tenen 3 vagons, el RI no existeix en aquests trens"""
+        if train_type in ['113', '213', '213x2']: #113 L7, 213 llobregat-anoia
+            self._carriers['RI'] = 'Vagó inexistent'
+
 
     # Getters
     def get_line(self):
@@ -39,6 +44,5 @@ class Train:
 
     def set_position(self, position):
         self._position = position
-
 
 
