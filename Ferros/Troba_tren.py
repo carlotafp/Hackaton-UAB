7cd7be7fbf763d4291efa4d2c546de97babdffc1
GuillemBalmanya <115980467+GuillemBalmanya@users.noreply.github.com>
2024-05-19 11:04:22 +0200

    Update Troba_tren.py

--- Ferros/Troba_tren.py
+++ Ferros/Troba_tren.py
@@ -4,20 +4,26 @@ import time
 import json
 
 
-
 def queden_minusvalids(dicc):
-    return not(all(element["minusvalid"] == 0 for element in dicc["carriers"].values()))
+    return not (all(element["minusvalid"] == 0 for element in dicc["carriers"].values()))
+
 
 def queden_reservats(dicc):
-    return not(all(element["reservats"] == 0 for element in dicc["carriers"].values()))
+    return not (all(element["reservats"] == 0 for element in dicc["carriers"].values()))
+
 
 def troba_tren(parada_inical, parada_final, sindrome):
-
     with open('trens.json', 'r') as f:
         ll_trens = json.load(f)
 
-    dic_parades = {"Plaça Catalunya":"PC", "Provença": "PR", "Gràcia": "GR", "Sant Gervasi": "SG", "Muntaner": "MN", "La Bonanova": "BN", "Les Tres Torres": "TT", "Sarrià": "SR", "Peu del Funicular": "PF", "Baixador de Vallvidrera": "VL", "Les Planes": "LP", "La Floresta": "LF", "Valldoreix": "VD", "Sant Cugat Centre": "VD", "Volpelleres": "VO", "Sant Joan": "SJ", "Bellaterra": "BT", "Universitat Autònoma": "UN", "Sant Quirze": "SQ", "Can Feu | Gràcia": "CF", "Sabadell Plaça Major": "PJ", "La Creu Alta": "CT", "Sabadell Nord": "NO", "Sabadell Parc del Nord": "PN"}
-    
+    dic_parades = {"Plaça Catalunya": "PC", "Provença": "PR", "Gràcia": "GR", "Sant Gervasi": "SG", "Muntaner": "MN",
+                   "La Bonanova": "BN", "Les Tres Torres": "TT", "Sarrià": "SR", "Peu del Funicular": "PF",
+                   "Baixador de Vallvidrera": "VL", "Les Planes": "LP", "La Floresta": "LF", "Valldoreix": "VD",
+                   "Sant Cugat Centre": "VD", "Volpelleres": "VO", "Sant Joan": "SJ", "Bellaterra": "BT",
+                   "Universitat Autònoma": "UN", "Sant Quirze": "SQ", "Can Feu | Gràcia": "CF",
+                   "Sabadell Plaça Major": "PJ", "La Creu Alta": "CT", "Sabadell Nord": "NO",
+                   "Sabadell Parc del Nord": "PN"}
+
     inicial = dic_parades[parada_inical]
     final = dic_parades[parada_final]
     direccio = None
@@ -25,11 +31,11 @@ def troba_tren(parada_inical, parada_final, sindrome):
     for value in dic_parades.values():
         if value == inicial:
             direccio = "A"
-            break   
+            break
         if value == final:
             direccio = "D"
             break
-    
+
     final = None
     minim = float("inf")
 
@@ -46,9 +52,17 @@ def troba_tren(parada_inical, parada_final, sindrome):
                     if cont < minim:
                         final, minim = tren, cont
 
-    return train.Train(final["line"], final["ID"], final["direction"], final["stops"], final["train_type"], final["position"], int(list(final["carriers"].values())[0]["percent"]), int(list(final["carriers"].values())[0]["percent"]), int(list(final["carriers"].values())[0]["percent"]), int(list(final["carriers"].values())[0]["percent"]))
+    selected_train = train.Train(final["line"], final["ID"], final["direction"], final["stops"], final["train_type"],
+                                 final["position"], int(list(final["carriers"].values())[0]["percent"]),
+                                 int(list(final["carriers"].values())[0]["percent"]),
+                                 int(list(final["carriers"].values())[0]["percent"]),
+                                 int(list(final["carriers"].values())[0]["percent"]))
+    if sindrome in ["Cadira de rodes", "Cotxet"]:
+        selected_train.decrementa_minusvalids()
+    if sindrome in ["Lisiat/da", "Embaraçada", "3a Edat", "Família"]:
+        selected_train.decrementa_reservats()
 
-
+    return selected_train
 
 
 if __name__ == "__main__":
@@ -59,4 +73,6 @@ if __name__ == "__main__":
     for _ in range(20):
         cotxe = tren.decrementa_minusvalids()
         print(tren, "\n", cotxe, "\n------------")
+
+
 
